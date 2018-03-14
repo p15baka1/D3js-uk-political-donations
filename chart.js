98736f38abee396f88aaf66fc2091187867b6cdb
p15baka1 <p15baka1@ionio.gr>
2018-03-14 23:33:15 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -96,8 +96,8 @@ function transition(name) {
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
-		$("#view-source-type").fadeIn(1000);
-		$("#view-donation-amount").fadeOut(1000);
+		$("#view-source-type").fadeOut(1000);
+		$("#view-donation-amount").fadeIn(250);
 		return donationType();
 	}
 }
